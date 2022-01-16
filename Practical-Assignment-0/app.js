6008bf7360ae0d0f507191434015ace696ff5ac4
adarshguptacse18 <adarshgupta.cse18@itbhu.ac.in>
2022-01-16 17:32:50 +0530

    Assignment-0

---
+++ Practical-Assignment-0/app.js
@@ -0,0 +1,24 @@
+function isLower(c) {
+    return (c >= 'a' && c <= 'z');
+}
+function isUpper(c) {
+    return (c >= 'A' && c <= 'Z');
+}
+// This functions a character x such that (x + c) == 25 (0 based indexing)
+function calculateComplement(c) {
+    let base;             
+    if(isUpper(c)) {         //lowercase
+        base = 65;
+    } else if(isLower(c)) {  //uppercase
+        base = 97;
+    }
+    else return c;                                         //returns the same if the character is not a english alphabet
+    const resCode = 25 - (c.charCodeAt(0) - base) + base;  // apply our logic and add base value to the resulting value
+    return String.fromCharCode(resCode);                   // returns the character at that ascii value
+}
+// this function returns applies the logic to whole code
+const encodeDecode = (str) => [...str].map(calculateComplement).join('');
+
+const [ plainInput, encryptedInput ] = document.getElementsByName('encryptData');
+plainInput.oninput = () => encryptedInput.value = encodeDecode(plainInput.value);
+encryptedInput.oninput = () => plainInput.value = encodeDecode(encryptedInput.value);
